a09f2326f1ae01e6d8f6b99ecd44f013e5cde8bb
Iana <77331186+ianalad@users.noreply.github.com>
2021-07-05 22:50:46 +0300

    Main file

--- Corona_Vaccine.py
+++ Corona_Vaccine.py
@@ -15,8 +15,13 @@ from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import NoSuchElementException
 
+from dotenv import load_dotenv
+
+
+load_dotenv() # This loads the environmental variables from the .env file in the root folder
+
 TELEGRAM_BOT_ID = ''
-# TELEGRAM_SUPERGROUP_ID must be a number
+#TELEGRAM_SUPERGROUP_ID must be numerical
 TELEGRAM_SUPERGROUP_ID = 
 URL = 'https://siunsote.eaika.fi/koronarokote'
 
@@ -25,18 +30,17 @@ options.headless = True
 d = webdriver.Firefox()
 d.get(URL)
 
-# Check for Siilainen, Joensuu
+# Part that needs to be scraped
 elem = d.find_element_by_class_name('row').click()
 elem = d.find_element_by_class_name('col-md-12').click()
 elem = d.find_element_by_id('channel_13_119').click()
 
-
 def send(telegram_data, TELEGRAM_CHAT_ID, TELEGRAM_BOT_ID):
-            """
-            TELEGRAM_CHAT_ID must be a number!
-            """
-            bot = telegram.Bot(token=TELEGRAM_BOT_ID)
-            bot.sendMessage(chat_id=TELEGRAM_CHAT_ID, text=telegram_data)
+    """
+    TELEGRAM_CHAT_ID must be a number!
+    """
+    bot = telegram.Bot(token=TELEGRAM_BOT_ID)
+    bot.sendMessage(chat_id=TELEGRAM_CHAT_ID, text=telegram_data)
 
 def check_exists_by_xpath(xpath):
     try:
@@ -45,50 +49,32 @@ def check_exists_by_xpath(xpath):
         return False
     return True
 
-try:
-    #Check for Siilainen, Joensuu
-    elem = check_exists_by_xpath('//*[@id="channel_13_206"]/span')
-    
-    if elem == False:
+xpath_list = ['//*[@id="channel_13_206"]/span', '//*[@id="channel_13_190"]/span']
+location = ['Siilainen, Joensuu', 'Rääkkylä']
+
+for i in range(len(xpath_list)):
+    try:
+        if i == 1:
+            elem = d.find_element(By.XPATH, '//*[@id="landing_vaccination"]/div/div[1]/button[1]').click()
+            elem = d.find_element_by_id('channel_13_114').click()
+                
+        elem = check_exists_by_xpath(xpath_list[i])
         
-        telegram_data = "Moi! Vaccination time is available in Siilainen, Joensuu. Check out: https://siunsote.eaika.fi/koronarokote"
-        send(telegram_data, TELEGRAM_SUPERGROUP_ID, TELEGRAM_BOT_ID)
-        print("Time is available in Siilainen, Joensuu")
-        d.close() #Close the connection in a browser
-        
-    else:
-        
-        elem = d.find_element(By.XPATH, '//*[@id="landing_vaccination"]/div/div[1]/button[1]').click()
-        elem = d.find_element_by_id('channel_13_114').click()
-        
-        try: 
-            #Check for Rääkkylä
-            elem = check_exists_by_xpath('//*[@id="channel_13_190"]/span') 
+        if elem == False:
             
-            if elem == False:
-                
-                telegram_data = "Moi! Vaccination time is available in Rääkkylä. Check out: https://siunsote.eaika.fi/koronarokote"
-                send(telegram_data, TELEGRAM_SUPERGROUP_ID, TELEGRAM_BOT_ID)
-                print("Time is available in Rääkkylä")
-                d.close() #Close the connection in a browser
-                
-            else:
-                d.close() #Close the connection in a browser
-                print("No time is available in Siilainen, Joensuu and Rääkkylä")
-        except WebDriverException:
+            telegram_data = "Moi! Vaccination time is available in " + location[i] + "." + " Check out: https://siunsote.eaika.fi/koronarokote"
+            send(telegram_data, TELEGRAM_SUPERGROUP_ID, TELEGRAM_BOT_ID)
+            print("Time is available in " + location[i])
             d.close() #Close the connection in a browser
-            print("Error detected")
-except WebDriverException:
-    d.close() #Close the connection in a browser
-    print("Error detected")
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-
+            
+            if i == 0:
+                break
+            
+        else:
+            if i == 1:
+                d.close() #Close the connection in a browser
+            print("No time is available in " + location[i])
+            
+    except WebDriverException:
+        d.close() #Close the connection in a browser
+        print("Error detected")
